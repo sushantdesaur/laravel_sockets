e22ca10871403d0e16ba4b5de99dc76c83423923
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2021-12-02 10:53:04 +0530

    created user relationships

--- app/Message.php
+++ app/Message.php
@@ -5,6 +5,16 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
-{
-    //
+{   
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = ['message'];
+    
+    //Create user relation function
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2021_12_02_050912_create_messages_table.php
+++ database/migrations/2021_12_02_050912_create_messages_table.php
@@ -14,7 +14,9 @@ class CreateMessagesTable extends Migration
     public function up()
     {
         Schema::create('messages', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->text('message');
             $table->timestamps();
         });
     }
