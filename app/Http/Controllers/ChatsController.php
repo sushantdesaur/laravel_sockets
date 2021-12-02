9e5d3100373033da21240c4e68aadda4fc97b873
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2021-12-02 11:00:47 +0530

    created user authentication in ChatsController

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ChatsController extends Controller
 {
-    //
+    //Constructor function so that authenticated user can go to chats route
+    public function __construct() {
+        $this->middleware('auth');
+    }
+    public function index() {
+        return view('chats');
+    }
 }
